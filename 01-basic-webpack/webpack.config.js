3b5f371a8fba79b406a8424196119de110d59b4b
lesenelir <miaomiaobabyzy@gmail.com>
2022-06-08 19:01:55 +0800

    feat: webpack resource configuration

--- 01-basic-webpack/webpack.config.js
+++ 01-basic-webpack/webpack.config.js
@@ -6,12 +6,14 @@ module.exports = {
   // 输出
   output: {
     path: path.resolve(__dirname, "dist"), // __dirname 表示当前文件的文件夹
-    filename: 'main.js' // 输出文件名
+    filename: 'static/js/main.js', // js输出文件名
+    clean: true // 自动清除打包的文件 - webpack4 中的 cleanMyPlugin插件
   },
   // 加载器
   module: {
+    // loader的配置
     rules: [
-      // loader的配置
+      // 处理css资源
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
@@ -27,7 +29,30 @@ module.exports = {
       {
         test: /\.styl$/,
         use: ['style-loader', 'css-loader', 'stylus-loader']
-      }
+      },
+      // 处理图片资源
+      {
+        test: /\.(png|jpe?g|git|webp)$/,
+        type: "asset",
+        parser: { // 对图片资源进行优化
+          dataUrlCondition: {
+            maxSize: 10 * 1024 // 小于10kb的图片会被base64处理
+          }
+        },
+        generator: {
+          // 修改图片打包后输出的文件路径
+          filename: 'static/images/[hash:8][ext][query]'
+        }
+      },
+      // 处理字体资源 + 其他资源(视频资源等)
+      {
+        test: /\.(ttf|woff2?|mp4|mp3|avi)$/,
+        type: "asset/resource",
+        generator: {
+          // 修改字体打包后输出的文件路径
+          filename: 'static/media/[hash:8][ext][query]'
+        }
+      },
     ]
   },
   // 插件
