f080d5b13294896aae60dba83b49d745d2352141
lesenelir <miaomiaobabyzy@gmail.com>
2022-06-13 12:19:18 +0800

    feat: webpack packing speed configuration

--- 01-basic-webpack/webpack.config.js
+++ 01-basic-webpack/webpack.config.js
@@ -1,7 +1,11 @@
+const os = require('os')
 const path = require('path')
 const ESLintWebpackPlugin = require('eslint-webpack-plugin')
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
+
+const threads = os.cpus().length // 获取cpu核数
 
 module.exports = {
   // 入口
@@ -16,111 +20,130 @@ module.exports = {
   module: {
     // loader的配置
     rules: [
-      // 处理css资源
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
+        // 每个文件只能被其中一个loader配置处理
+        oneOf: [
+          // 处理css资源
           {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  "postcss-preset-env", // 能解决大多数样式兼容性问题
-                ],
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              {
+                loader: "postcss-loader",
+                options: {
+                  postcssOptions: {
+                    plugins: [
+                      "postcss-preset-env", // 能解决大多数样式兼容性问题
+                    ],
+                  },
+                },
+              }
+            ]
+          },
+          {
+            test: /\.less$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              {
+                loader: "postcss-loader",
+                options: {
+                  postcssOptions: {
+                    plugins: [
+                      "postcss-preset-env", // 能解决大多数样式兼容性问题
+                    ],
+                  },
+                },
               },
+              'less-loader'
+            ]
+          },
+          {
+            test: /\.s[ac]ss$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              {
+                loader: "postcss-loader",
+                options: {
+                  postcssOptions: {
+                    plugins: [
+                      "postcss-preset-env", // 能解决大多数样式兼容性问题
+                    ],
+                  },
+                },
+              },
+              'sass-loader'
+            ]
+          },
+          {
+            test: /\.styl$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              {
+                loader: "postcss-loader",
+                options: {
+                  postcssOptions: {
+                    plugins: [
+                      "postcss-preset-env", // 能解决大多数样式兼容性问题
+                    ],
+                  },
+                },
+              },
+              'stylus-loader'
+            ]
+          },
+          // 处理图片资源
+          {
+            test: /\.(png|jpe?g|git|webp)$/,
+            type: "asset",
+            parser: { // 对图片资源进行优化
+              dataUrlCondition: {
+                maxSize: 10 * 1024 // 小于10kb的图片会被base64处理
+              }
             },
+            generator: {
+              // 修改图片打包后输出的文件路径
+              filename: 'static/images/[hash:8][ext][query]'
+            }
+          },
+          // 处理字体资源 + 其他资源(视频资源等)
+          {
+            test: /\.(ttf|woff2?|mp4|mp3|avi)$/,
+            type: "asset/resource",
+            generator: {
+              // 修改字体打包后输出的文件路径
+              filename: 'static/media/[hash:8][ext][query]'
+            }
+          },
+          // 处理js资源
+          {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: [
+              {
+                // 为babel开启多线程 loader
+                loader: 'thread-loader',
+                options:  {
+                  works: threads
+                }
+              },
+              {
+                loader: 'babel-loader',
+                options: {
+                  // 可以替代为babel.config.js
+                  // presets: ['@babel/preset-env']
+                  // 开启babel缓存
+                  cacheDirectory: true,
+                  // 关闭缓存文件压缩
+                  cacheCompression: false
+                }
+              }
+            ]
           }
         ]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  "postcss-preset-env", // 能解决大多数样式兼容性问题
-                ],
-              },
-            },
-          },
-          'less-loader'
-        ]
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  "postcss-preset-env", // 能解决大多数样式兼容性问题
-                ],
-              },
-            },
-          },
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.styl$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  "postcss-preset-env", // 能解决大多数样式兼容性问题
-                ],
-              },
-            },
-          },
-          'stylus-loader'
-        ]
-      },
-      // 处理图片资源
-      {
-        test: /\.(png|jpe?g|git|webp)$/,
-        type: "asset",
-        parser: { // 对图片资源进行优化
-          dataUrlCondition: {
-            maxSize: 10 * 1024 // 小于10kb的图片会被base64处理
-          }
-        },
-        generator: {
-          // 修改图片打包后输出的文件路径
-          filename: 'static/images/[hash:8][ext][query]'
-        }
-      },
-      // 处理字体资源 + 其他资源(视频资源等)
-      {
-        test: /\.(ttf|woff2?|mp4|mp3|avi)$/,
-        type: "asset/resource",
-        generator: {
-          // 修改字体打包后输出的文件路径
-          filename: 'static/media/[hash:8][ext][query]'
-        }
-      },
-      // 处理js资源
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader'
-          // options: { // 可以替代为babel.config.js
-          //   presets: ['@babel/preset-env']
-          // }
-        }
       }
     ]
   },
@@ -129,7 +152,14 @@ module.exports = {
     // 插件配置
     new ESLintWebpackPlugin({
       // 指定检查文件的根目录
-      context: path.resolve(__dirname, "src")
+      context: path.resolve(__dirname, "src"),
+      exclude: "node_modules",
+      cache: true,
+      cacheLocation: path.resolve(
+          __dirname,
+          "./node_modules/.cache/eslintcache"
+      ),
+      threads // 为ESLint开启多线程
     }),
     new HtmlWebpackPlugin({
       // 以 public/index.html 为模板创建文件
@@ -137,14 +167,26 @@ module.exports = {
       template: path.resolve(__dirname, "public/index.html"),
     }),
     // css 压缩
-    new CssMinimizerPlugin()
+    // new CssMinimizerPlugin()
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // css压缩也可以写到optimization.minimizer里面，效果一样的
+      new CssMinimizerPlugin(),
+      // 当生产模式会默认开启TerserPlugin，但是我们需要进行其他配置，就要重新写了
+      new TerserPlugin({
+        parallel: threads // 开启多进程
+      })
+    ],
+  },
   // 开发服务器 (npx webpack serve - 指令发生改变)
   // 开发服务器是在内存中打包，不会输出资源（不会产生dist目录）
   devServer: {
     host: "localhost", // 启动服务器域名
     port: "8080", // 启动服务器端口号
     open: true, // 是否自动打开浏览器
+    hot: true // 开启HMR
   },
   // 模式
   mode: "development",
